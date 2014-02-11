333c5f6a909bce17e2678268b84a222f2bac594c
SylvieC <sylviecottrell@gmail.com>
2014-02-11 13:17:13 -0800

    added a users_controller_spec.rb

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -1,8 +1,34 @@
-require '/spec_helper'
+require 'spec_helper'
 
-# describe "Cards" do
-# 	 it "should be successful" do
-# 	 	get home_path
-# 	 	response.status.should == 200
-# end
-# end
+describe "CardsController" do
+
+	 describe 'GET practice' do
+	    
+
+		   it "should render :practice template" do
+		   get :practice
+		   expect(response).to render_template("practice")
+		 end
+		end
+
+		
+
+
+   describe "POST create" do
+
+	   	before(:each) do
+	   		@card = mock_model("Card")
+	   		allow(Card).to receive(:create)
+	   	end
+
+	   	it "should create a Card" do
+	   		post :create, {card:{title: "DSL",content: "Domain Specific Language"}}
+	   		expect(response).to redirect_to("/reserve")
+	   	end
+
+   end
+
+
+
+end
+
--- spec/requests/cards_spec.rb
+++ spec/requests/cards_spec.rb
@@ -1,10 +1,18 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe "Cards"  do
-# 	describe "GET /practice"
-# 	it " should be successful" do
-# 		get practice_path
-# 		response.status.shoud == 200
-# end
+describe "Cards"  do
+ 	describe "GET /practice"
+ 	it " should be successful" do
+ 		get :practice
+ 		response.should be_success
+	 end
 	
-# end
+
+
+
+ it "should have the right title" do
+   get 'practice'
+   response.should have_tag( "h1", "The Practice Pile")
+    
+ end
+end
