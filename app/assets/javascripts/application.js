f695169d2aef93aa83bbba448c9772188c84c0c9
SylvieC <sylviecottrell@gmail.com>
2014-05-14 18:57:09 -0700

    took out unnecessary console.log

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,7 +5,6 @@ $(window).load(function(){
   $("body").on("click","#save", function(ev){
     $("#count_form").submit();
   });
-  console.log("hello");
    $(".answer").hide();
  
     $(".front").click(function(){
@@ -13,7 +12,6 @@ $(window).load(function(){
       count += 1 ;
      $("#" + cardId).show();
      if(count >= length) {
-      console.log("FIRED EVENT")
       $("body").append("<div class='save-con'><button id='save' class='btn btn-danger'>SAVE PRACTICE</button></div>");
 
     }
